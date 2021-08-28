e279044f6ee717896d1b6ffbe914b01770afbb21
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-08-28 16:21:53 -0500

    created html and js files

---
+++ js/game.js
@@ -0,0 +1,15 @@
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+console.log("this logs a string, good for leaving yourself a message");
+//this will do math and log 20
+console.log(10 + 10);
+//This is adding the variable into the string:
+console.log("Our rotor's name is " + playerName);
+
+// function fight() {
+//   window.alert("The fight has begun!");
+// }
+
+//fight();
