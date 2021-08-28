25ccbba904f46f76cdacbdaada3cc6b04f40767e
Jeff Anderson-Lester <viperhq@hotmail.com>
2021-08-28 17:06:03 -0500

    added robot attack and score keeping logic

--- js/game.js
+++ js/game.js
@@ -68,3 +68,17 @@ console.log(
     enemyHealth +
     " health remaining."
 );
+
+// check player's health
+if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+} else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
+}
+
+// check enemy's health
+if (enemyHealth <= 0) {
+  window.alert(enemyName + " has died!");
+} else {
+  window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
